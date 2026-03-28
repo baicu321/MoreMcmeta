3d1eac7aade0489db17643130099ab09a37e4d81
cu6 <3077476144@qq.com>
2026-03-28 16:42:54 +0800

    gradle and runs update why arch just like idiot?????????

---
+++ fabric/src/main/java/committee/nova/moremcmeta/fabric/MoreMcmetaFabric.java
@@ -0,0 +1,12 @@
+package committee.nova.moremcmeta.fabric;
+
+import committee.nova.moremcmeta.impl.client.MoreMcmeta;
+import net.fabricmc.api.ModInitializer;
+
+public final class MoreMcmetaFabric implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ fabric/src/main/java/committee/nova/moremcmeta/fabric/client/MoreMcmetaFabricClient.java
@@ -0,0 +1,10 @@
+package committee.nova.moremcmeta.fabric.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class MoreMcmetaFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
